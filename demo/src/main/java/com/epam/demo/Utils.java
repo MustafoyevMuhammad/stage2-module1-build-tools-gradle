50a382b2957bef7b99796ad86b58e304cd8eadbc
Mustafoyev <mmustafoyev9@gmail.com>
2022-11-04 15:45:48 +0500

    commit message

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,12 +5,18 @@ import com.epam.utils.StringUtils;
 import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.util.List;
+import java.util.function.Predicate;
 
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        return args.stream().map(StringUtils::isPositiveNumber).isParallel();
+        return args.stream().filter(Predicate.not(StringUtils::isPositiveNumber)).findFirst().isEmpty();
+
     }
 
+    public static void main(String[] args) {
+        List<String> list = List.of("11.22","2", "33","87");
+        System.out.println(isAllPositiveNumbers(list));
+    }
 
 }
