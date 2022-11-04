1cd6d0fbdf7dd3ad9a01af47f3cb75c7536c232a
Mustafoyev <mmustafoyev9@gmail.com>
2022-11-04 15:10:17 +0500

    commit message

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -8,10 +8,14 @@ import java.text.ParseException;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         try {
-            return NumberUtils.toInt(str) % 2 == 0;
+            return NumberUtils.toDouble(str) >= 0;
         }
         catch (Exception e){
             return false;
         }
     }
+    public static void main(String[] args) {
+        System.out.println(isPositiveNumber("11.22"));
+    }
 }
+
